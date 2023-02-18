39c8ee07128f1b99d394b8d5b2c25c48a4b60911
Gerti <GertiGoga@gmail.com>
2023-02-18 14:54:21 -0800

    Fixed API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import searchImages from "./API/searchImages";
 import ImageList from "./components/ImageList";
+import SearchBar from "./components/SearchBar";
+import ImageShow from "./components/ImageShow";
 
 function App() {
+  const handleSubmit = async (userSearch) => {
+    const result = await searchImages(userSearch);
+    ImageList(result);
+  };
+
   return (
     <div>
-      <ImageList />
+      <SearchBar onSubmit={handleSubmit} />
+      <ImageShow />
     </div>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,33 @@
-function SearchBar() {
-    return <div>SearchBar</div>
+import { useState } from "react";
+
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(term);
+  };
+
+  const handleChange = (e) => {
+    setTerm(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input is-primary"
+          type="text"
+          placeholder="Primary input"
+          onChange={handleChange}
+          value={term}
+        ></input>
+        <div onClick={handleSubmit} className="buttons">
+          <button className="button is-primary">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
